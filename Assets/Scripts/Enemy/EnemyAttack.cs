6c6c9f1606c8547cc2c3908635864644477f668a
Abizar Bagasrawala <Abizar@TherapyOSs-MacBook-Pro.local>
2017-12-14 15:54:11 +0530

    complete

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timeLastAttack;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();
+    }
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+		if(playerInRange && (Time.time - timeLastAttack) >= timeBetweenAttacks && enemyHealth.currentHealth > 0)
+        {
+            Attack ();
+        }
+
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+
+
+    void Attack ()
+    {
+		timeLastAttack = Time.time;
+
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
